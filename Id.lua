17612277d2330c22d446b0ed6d8ebc7e83e1cded
YirdeX-Dev <3939691930@qq.com>
2026-07-03 23:31:30 +0800

    Add files via upload

---
+++ Id.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 142823291 then --- 服务器id
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YIRDEX/scripts/refs/heads/main/Game/YX-HubMM2%E5%8A%A0%E5%AF%86%E8%84%9A%E6%9C%AC.lua"))()---该服务器脚本
+
+elseif game.PlaceId == 572725757277227 then 
+
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YirdeX-Dev/scripts/refs/heads/main/YX-ChinacHub.lua"))()
+end
