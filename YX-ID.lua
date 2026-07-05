b347f1e1a0a99790bd387cc85aee50b8a7c1f95b
YirdeX-Dev <3939691930@qq.com>
2026-07-05 16:36:38 +0800

    Delete 自动甩人.luag

--- YX-ID.lua
+++ YX-ID.lua
@@ -4,5 +4,5 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/YIRDEX/scripts/refs/h
 elseif game.PlaceId == 572725757277227 then 
 
 else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YirdeX-Dev/scripts/refs/heads/main/YX-ChinacHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YirdeX-Dev/scripts/refs/heads/main/YX-China-Hub.lua"))()
 end
