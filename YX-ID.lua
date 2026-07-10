1c29bab0d761cab245a10517658523b315c4864d
YirdeX-Dev <3939691930@qq.com>
2026-07-10 16:32:38 +0800

    Delete 自动甩人.luag

--- YX-ID.lua
+++ YX-ID.lua
@@ -1,8 +1,1 @@
-if game.PlaceId == 142823291 then --- 服务器id
-
-
-elseif game.PlaceId == 572725757277227 then 
-
-else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/YirdeX-Dev/scripts/refs/heads/main/YX-脚本中心.lua"))()
-end
