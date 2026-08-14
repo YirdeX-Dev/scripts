d959d441d5997c611c383edaccbca2c049b5cdd9
YirdeX-Dev <3939691930@qq.com>
2026-08-14 20:00:30 +0800

    Create YX-守卫.lua

---
+++ YX-守卫.lua
@@ -0,0 +1,11 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+  Title = " YX-守卫通知",
+  Text = "脚本已更换，请加入群聊，获得新脚本",
+  Icon = "rbxassetid://78122775411768",
+  Duration = 5,
+  Callback = bindable,
+  Button1 = "AF脚本群",
+  setclipboard("938714427")
+  Button2 = "YX脚本群(推荐)",
+  setclipboard("2152069753")
+})
